ebfe6b0c062214a9c3708052674dec7dba76c371
spltxt <94641349+spltxt@users.noreply.github.com>
2022-04-06 14:45:37 +0300

    Delete start.bat

--- start.bat
+++
@@ -1,5 +0,0 @@
-chcp 65001
-@echo off
-CALL C:\Users\Анатолий\Desktop\9.2\discordbot\venv\Scripts\activate.bat
-C:\Users\Анатолий\Desktop\9.2\discordbot\venv\Scripts\python.exe "C:\Users\Анатолий\Desktop\9.2\discordbot\bot.py"
-pause
